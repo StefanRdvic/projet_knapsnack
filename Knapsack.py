d49e196d894c28dcb7882c2381d99d90c9fd84bd
elkidd <stefan.rvic@gmail.com>
2021-12-08 10:50:14 +0100

    initial project

---
+++ Knapsack.py
@@ -0,0 +1,22 @@
+class Knapsack:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.content = []
+
+    def get_value_and_weight(self, objects_dict) -> (int, int):
+        pass
+
+    def print_content(self, objects_dict) -> None:
+        pass
+
+
+def solve_knapsack_greedy(knapsack, objects_dict) -> Knapsack:
+    pass
+
+
+def solve_knapsack_best(knapsack, objects_dict) -> Knapsack:
+    pass
+
+
+def solve_knapsack_optimal(knapsack, objects_dict) -> Knapsack:
+    pass
