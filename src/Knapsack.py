12aa6db503fa49f5ea7e0aebd2f0b61e7afaa0f3
elkidd <stefan.rvic@gmail.com>
2022-01-03 23:28:59 +0100

    add : Best solution + comment

--- src/Knapsack.py
+++ src/Knapsack.py
@@ -2,11 +2,26 @@ class Knapsack:
     def __init__(self, capacity):
         self.capacity = capacity
         self.content = []
+        # Tracking attributs used to get time on accessing knapsacks value and weight
+        self.available_capacity = capacity
+        self.current_value = 0
 
     def copy(self):
+        # Make a copy of the calling knapsack
         knapsack_copy = Knapsack(self.capacity)
         knapsack_copy.content.extend(self.content)
+        knapsack_copy.available_capacity = self.available_capacity
+        knapsack_copy.current_value = self.current_value
         return knapsack_copy
+
+    def add_item(self, item, objects_dict):
+        # Add an item to the knapsack content
+        # Precondition : the knapsack have the necessary capacity
+        assert not self.available_capacity - objects_dict[item][1] < 0
+        self.content.append(item)
+        self.available_capacity -= objects_dict[item][1]
+        self.current_value += objects_dict[item][0]
+        return self
 
     def get_value_and_weight(self, objects_dict) -> (int, int):
         total_value = 0
@@ -32,30 +47,79 @@ class Knapsack:
 
 
 # -------------------------------------------------------------------------
+# Utils
+# -------------------------------------------------------------------------
+def compare(knapsack1, knapsack2):
+    # Return the knapsack with the best value
+    # If both knapsacks have the same value, choose the one with the most available capacity
+    if knapsack1.current_value == knapsack2.current_value:
+        return knapsack1 if knapsack1.available_capacity > knapsack1.available_capacity else knapsack2
+
+    return knapsack1 if knapsack1.current_value >= knapsack2.current_value else knapsack2
+
+
+# -------------------------------------------------------------------------
 # Greedy solution
 # -------------------------------------------------------------------------
 def solve_knapsack_greedy(knapsack, objects_dict) -> Knapsack:
+    # the dictionary is sorted by the ratio between the value and the weight
     sorted_objects_dict = dict(sorted(objects_dict.items(), key=lambda i: (i[1][0] / i[1][1]), reverse=True))
-    current_sack_capacity = 0
 
     for item in sorted_objects_dict.keys():
-        if current_sack_capacity == knapsack.capacity:
+        if knapsack.available_capacity == 0:    # No space left = no need to continue
             return knapsack
 
         weight = objects_dict[item][1]
 
-        if weight + current_sack_capacity <= knapsack.capacity:
-            knapsack.content.append(item)
-            current_sack_capacity += weight
+        if weight <= knapsack.available_capacity:
+            knapsack.add_item(item, objects_dict)
 
     return knapsack
 
 
 # -------------------------------------------------------------------------
-# best solution
+# Best solution
 # -------------------------------------------------------------------------
 def solve_knapsack_best(knapsack, objects_dict) -> Knapsack:
-    pass
+    keys = list(objects_dict.keys())
+    return get_max_value_memoization(dict(), knapsack, objects_dict, keys, len(keys) - 1, knapsack.capacity)
+
+
+def get_max_value_memoization(mem, knapsack, objects_dict, keys, n, remaining):
+    """
+    Get the best knapsack, this method uses dynamic programing and memoization concept
+    :param mem: The calculated knapsack memory
+    :param knapsack: Current knapsack
+    :param objects_dict: Object dictionary
+    :param keys: List of the dictionary keys (used to work with indexes)
+    :param n: Number of item left
+    :param remaining: Number of space left before the knapsack filling
+    :return: the calculated knapsack with the best value
+    """
+
+    #  When the knapsack at nTh item and current remaining is already calculated simply returns it
+    if (n, remaining) in mem.keys():
+        return mem[(n, remaining)].copy()
+
+    if n < 0 or remaining == 0:
+        return knapsack.copy()
+
+    weight = objects_dict[keys[n]][1]
+
+    # Follow the same pattern as the Brute-Force method
+    # The stored knapsacks has to be copied when accessed to avoid overwriting
+
+    if weight > remaining:
+        return get_max_value_memoization(mem, knapsack.copy(), objects_dict, keys, n - 1, remaining)
+
+    knapsack_include = get_max_value_memoization(
+        mem, knapsack, objects_dict, keys, n - 1, remaining - weight
+    ).add_item(keys[n], objects_dict)   # Add the item after the processing
+
+    knapsack_exclude = get_max_value_memoization(mem, knapsack, objects_dict, keys, n - 1, remaining)
+
+    mem[(n, remaining)] = compare(knapsack_include, knapsack_exclude)   # Store the best calculated value
+    return mem[(n, remaining)].copy()
 
 
 # -------------------------------------------------------------------------
@@ -63,36 +127,32 @@ def solve_knapsack_best(knapsack, objects_dict) -> Knapsack:
 # -------------------------------------------------------------------------
 def solve_knapsack_optimal(knapsack, objects_dict):
     keys = list(objects_dict.keys())
-    return get_max_value(knapsack, objects_dict, keys, knapsack.capacity, 0)
+    return get_max_value(knapsack, objects_dict, keys, 0)
 
 
-def get_max_value(knapsack, objects_dict, keys, current_capacity, current_index):
-    if current_index == len(keys) or current_capacity <= 0:
+def get_max_value(knapsack, objects_dict, keys, current_index):
+    """
+    Brute-force algorithm checking each individual knapsack solution and landing the best one
+    :param knapsack: current knapsack
+    :param objects_dict: objects dictionary
+    :param keys: List of the dictionary keys (used to work with indexes)
+    :param current_index: the current index lol
+    :return: the best knapsack solution within all possibilities
+    """
+    if current_index == len(keys) or knapsack.available_capacity == 0:
         return knapsack
 
     weight = objects_dict[keys[current_index]][1]
-    knapsack_copy = knapsack.copy()
 
-    if weight <= current_capacity:
-        knapsack.content.append(keys[current_index])
+    # Follow the simple pattern : if the item can be added -> add it or don't then continue
+    # End with a comparison between the two path
+    if weight <= knapsack.available_capacity:
+        knapsack_copy = knapsack.copy().add_item(keys[current_index], objects_dict)
 
-        knapsack_include = get_max_value(
-            knapsack, objects_dict, keys, current_capacity - weight, current_index + 1
-        )
+        knapsack_include = get_max_value(knapsack_copy, objects_dict, keys, current_index + 1)
+        knapsack_exclude = get_max_value(knapsack, objects_dict, keys, current_index + 1)
 
-        knapsack_exclude = get_max_value(
-            knapsack_copy, objects_dict, keys, current_capacity, current_index + 1
-        )
-        return get_best(knapsack_include, knapsack_exclude, objects_dict)
+        return compare(knapsack_include, knapsack_exclude)
 
-    return get_max_value(knapsack_copy, objects_dict, keys, current_capacity, current_index + 1)
-
-
-def get_best(knapsack1, knapsack2, objects_dict):
-    sack1_value, sack1_weight = knapsack1.get_value_and_weight(objects_dict)
-    sack2_value, sack2_weight = knapsack2.get_value_and_weight(objects_dict)
-
-    if sack1_value == sack2_value:
-        return knapsack1 if sack1_weight < sack2_weight else knapsack2
-
-    return knapsack1 if sack1_value > sack2_value else knapsack2
+    # If it can't be added just go to the next item without the processing
+    return get_max_value(knapsack, objects_dict, keys, current_index + 1)
